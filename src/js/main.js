01088c380bf213534acb80a1e5a108f57badf0b1
Jie Zheng <prettykingking@live.com>
2023-08-09 19:21:02 +0800

    feat: stem using KaTex

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 import Header from "./header.js";
 
 function main() {
-  new Header(document.querySelector(".header-shadow"), {});
+  const header = document.querySelector(".header-shadow");
+  if (header !== null) {
+    new Header(header, {});
+  }
 }
 
 export default main;
