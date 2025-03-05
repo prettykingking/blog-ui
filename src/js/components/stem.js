c12e44f32de175bf01f50660632e2f69e514efbe
Jie Zheng <prettykingking@live.com>
2025-03-05 14:58:18 +0800

    fix: refactor STEM component

--- src/js/components/stem.js
+++ src/js/components/stem.js
@@ -5,20 +5,20 @@ import EventHandler from "bootstrap/js/src/dom/event-handler.js";
 
 class Stem {
   constructor() {
-    this.setup();
+    EventHandler.on(document, "DOMContentLoaded", () => {
+      this.setup();
+    });
   }
 
   setup() {
-    EventHandler.on(document, "DOMContentLoaded", () => {
-      const spans = document.querySelectorAll("span.inline-stem");
-      for (const s of spans) {
-        renderMathInElement(s);
-      }
-      const blocks = document.querySelectorAll("div.stem");
-      for (const b of blocks) {
-        renderMathInElement(b);
-      }
-    });
+    const spans = document.querySelectorAll(".article span.inline-stem");
+    for (const s of spans) {
+      renderMathInElement(s);
+    }
+    const blocks = document.querySelectorAll(".article div.stem");
+    for (const b of blocks) {
+      renderMathInElement(b);
+    }
   }
 }
 
